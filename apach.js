826787cc7a08af08d1daa117452f13889102aad0
wupengforIT <wupengforIT@163.com>
2018-10-27 13:03:46 +0800

    修改请求bug

--- apach.js
+++ apach.js
@@ -12,24 +12,35 @@ server.on('request', function (request, response) {
                 return response.end('服务器请求错误');
             }
             content = content.toString();
-            fs.readdir(dir, function (err, files) {
-                var fileInfos = [];
-                if (err) {
-                    response.setHeader("Content-Type", "text/plain;charset=utf-8")
-                    return response.end('没有更多文件了。。。');
-                }
-                files.forEach(function (item) {
-                    var fileInfo = {};
-                    fileInfo.fileName = item;
-                    fileInfo.isFile = fs.statSync(dir + '/' + item).isFile();
-                    fileInfo.currentPath = (dir + '/' + item).replace('D:/www/', '');
-                    fileInfos.push(fileInfo);
-                });
-                var html = artTemplate.render(content, {
-                    files: fileInfos
-                });
-                response.end(html);
-            })
+            // 先判断是否为文件
+            if (fs.statSync(dir).isFile()) {
+                response.setHeader("Content-Type", "text/plain;charset=utf-8");
+                fs.readFile(dir, function (error, content) {
+                    if (error) {
+                        return response.end("文件不可读");
+                    }
+                    return response.end(content.toString());
+                })
+            } else {
+                fs.readdir(dir, function (err, files) {
+                    var fileInfos = [];
+                    if (err) {
+                        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+                        return response.end('没有更多文件了。。。');
+                    }
+                    files.forEach(function (item) {
+                        var fileInfo = {};
+                        fileInfo.fileName = item;
+                        fileInfo.isFile = fs.statSync(dir + '/' + item).isFile();
+                        fileInfo.currentPath = (request.url + '/' + item);
+                        fileInfos.push(fileInfo);
+                    });
+                    var html = artTemplate.render(content, {
+                        files: fileInfos
+                    });
+                    response.end(html);
+                })
+            }
         });
     }
 
